775f34a062d87f7226744499bca567ca2eaa93f8
HenryElendheim <henskielendheim@gmail.com>
2026-05-27 12:18:56 +0200

    Omar Code

---
+++ BookFilmInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Par_Prog_2
+{
+    internal class BookFilmInfo
+    {
+
+        public string Title;
+        public string Year;
+        public string Description;
+        public string Author;
+        public string Actor;
+
+        public BookFilmInfo(string title, string year, string description, string author, string actor)
+        {
+            Title = title;
+            Year = year;
+            Description = description;
+            Author = author;
+            Actor = actor;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"tittel: {Title}\n" +
+                $"ÅR: {Year}\n" +
+                $"Beskrivelse: {Description}\n" +
+                $"Forfatter/Regissør: {Author}\n" +
+                $"Skuespillere: {Actor}");
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using Par_Prog_2;
+
+List<BookFilmInfo> info = new List<BookFilmInfo>();
+bool addingproducts = true;
+while (addingproducts)
+{
+
+    Console.WriteLine("hva er tittelen på boka?");
+    string title = Console.ReadLine();
+
+
+    Console.WriteLine("hvilken år kom det ut");
+    string year = Console.ReadLine();
+
+    Console.WriteLine("skriv kort beskrivelse");
+    string description = Console.ReadLine();
+
+    Console.WriteLine("hvem er forfatteren/regisør?");
+    string author = Console.ReadLine();
+
+    Console.WriteLine("hvem er skuespilleren");
+    string actor = Console.ReadLine();
+
+    var bookfilminfo = new BookFilmInfo(title, year, description, author, actor);
+    string answer = Console.ReadLine();
+    if (answer == "N")
+    {
+        addingproducts = false;
+    }
+
+    Console.WriteLine("Vil du legge til flere bøker? (Y/N)");
+    for (int i = 0; i < info.Count; i++)
+    {
+        Console.WriteLine($"tittel: {title}\n" +
+                   $"ÅR: {year}\n" +
+                   $"Beskrivelse: {description}\n" +
+                   $"Forfatter/Regissør: {author}\n" +
+                   $"Skuespillere: {actor}");
+    }
+
+}
